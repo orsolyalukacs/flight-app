d758cf488b78a03ee5cfa18b528ef407dd7f34e4
Orsolya Lucas <orchie.lucas@gmail.com>
2018-12-16 23:03:25 +0100

    After selecting from first select, second is filtered to show only connections.

--- client/src/components/Select.vue
+++ client/src/components/Select.vue
@@ -2,19 +2,16 @@
 <template>
   <div class="select-group">
     <h3>Flights</h3>
-    <!-- <v-select :station="stations" label="shortName">
-      <template slot="station" slot-scope="station">
-        <span :class="station.shortName"></span>
-        {{ station.shortName }}
-      </template>
-    </v-select> -->
-    <select class="depart" v-model="departureStation">
-      <option :key='station' v-for="station in stations" :value="station">{{ station.shortName }}</option>
+    <select id="depart" v-model="depart" @change="update($event)">
+      <option :key='station.id' v-for="station in stations" :value="station">{{ station.shortName }}</option>
     </select>
 
-     <select class="arrive" v-model="arrivalStation">
-      <option :key='station' v-for="station in stations" :value="station">{{ station.shortName }}</option>
+     <select id="arrive" v-model="arrive">
+      <option :key='connection.id' v-for="connection in connections" :value="connection">{{ connection }}</option>
     </select>
+    <p v-if="depart&&arrive">
+        You selected a flight departing from {{ depart.shortName }} and going to {{ arrive }}.
+    </p>
   </div>
 </template>
 <script>
@@ -25,11 +22,9 @@ export default {
   data () {
     return {
       stations: [],
-      options: [
-        'foo',
-        'bar',
-        'baz'
-      ]
+      connections: [],
+      depart: [],
+      arrive: []
     }
   },
   created () {
@@ -37,7 +32,26 @@ export default {
     axios.get('https://mock-air.herokuapp.com/asset/stations')
       .then(function (response) {
         vm.stations = response.data
+        vm.cityName = {}
+        Object.keys(vm.stations).map(function (key, index) {
+          vm.cityName[vm.stations[index].iata] = vm.stations[index].shortName
+        })
+        // vm.stations.map(function (elem) {
+        //   return {
+        //     [elem.iata]: elem.shortName
+        //   }
+        // })
+        console.log(vm.cityName)
+        // console.log(vm.stations)
       })
+  },
+  methods: {
+    update (event) {
+      let vm = this
+      this.connections = this.depart.connections.map(function (elem) {
+        return vm.cityName[elem.iata]
+      })
+    }
   }
 }
 </script>
