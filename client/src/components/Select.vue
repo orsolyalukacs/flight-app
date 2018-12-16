dee1173739141a90cb7045626626282439f221a2
Orsolya Lucas <orchie.lucas@gmail.com>
2018-12-16 11:50:45 +0100

    Added Select Component and removed some unnecessary files.

---
+++ client/src/components/Select.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div class="select-group">
+    <h3>Flights</h3>
+    <!-- <v-select :station="stations" label="shortName">
+      <template slot="station" slot-scope="station">
+        <span :class="station.shortName"></span>
+        {{ station.shortName }}
+      </template>
+    </v-select> -->
+    <select class="depart" v-model="departureStation">
+      <option :key='station' v-for="station in stations" :value="station">{{ station.shortName }}</option>
+    </select>
+
+     <select class="arrive" v-model="arrivalStation">
+      <option :key='station' v-for="station in stations" :value="station">{{ station.shortName }}</option>
+    </select>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Select',
+  data () {
+    return {
+      stations: [],
+      options: [
+        'foo',
+        'bar',
+        'baz'
+      ]
+    }
+  },
+  created () {
+    let vm = this
+    axios.get('https://mock-air.herokuapp.com/asset/stations')
+      .then(function (response) {
+        vm.stations = response.data
+      })
+  }
+}
+</script>
+<style scoped>
+  li{
+    display: inline-block;
+    margin: 0 10px;
+  }
+  select{
+    width: 50%;
+    display: inline-block;
+  }
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from 'vue'
+import Router from 'vue-router'
+import VueSelect from 'vue-select'
+import Select from '@/components/Select'
 
-Vue.use(Router);
+Vue.use(Router)
+Vue.component('v-select', VueSelect.VueSelect)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-  ],
-});
+      name: 'root',
+      component: Select
+    }
+  ]
+})
