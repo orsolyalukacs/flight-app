f4d6cf930bba820d2c9d9891499922db3ea6af43
Orsolya Lucas <orchie.lucas@gmail.com>
2019-01-15 09:54:06 +0100

    Added moment to flight-summary to display date better.

--- client/src/components/FlightSummary.vue
+++ client/src/components/FlightSummary.vue
@@ -2,27 +2,27 @@
   <section class="summary">
     <div class="summary-text">
       <div class="depart-ticket" v-if="data.departTicket">
-        You selected:<br> a <em>{{ data.departTicket.bundle }}</em> ticket for {{ data.departTicket.price }} €
+        You selected:<br> a <em>{{ data.departTicket.bundle }}</em> ticket for {{ departPrice }} €
       </div>
       <p>
-        from <strong>{{$vnode.context.currentDepart.shortName}}</strong>
-        to <strong>{{$vnode.context.currentArrive.shortName}}</strong><br>
-        on {{$vnode.context.departDate.toISOString().split('T')[0]}} <br>
+        from <strong>{{ departName }}</strong>
+        to <strong>{{ returnName }}</strong><br>
+        on {{ departDateFormatted | day }} <br>
       </p>
       <p class="return-ticket" v-if="data.returnTicket.price">
         and a <br>
         <em>{{ data.returnTicket.bundle }}</em> return ticket for {{ data.returnTicket.price }} € <br>
-        coming back to <strong>{{$vnode.context.currentArrive.shortName}}</strong> on <br>
-        <span v-if="data.returnTicket.price">{{$vnode.context.returnDate.toISOString().split('T')[0]}}. </span>
+        coming back to <strong>{{ returnName }}</strong> on <br>
+        <span v-if="data.returnTicket.price">{{ returnDateFormatted | day }}. </span>
       </p>
     </div>
     <div class="totalsum">
         <h4>Total price:
-            <span v-if="data.departTicket.price && !data.returnTicket.price">
-                {{data.departTicket.price}} €
+            <span v-if="departPrice && !returnPrice">
+                {{ departPrice }} €
             </span>
-            <span v-else-if="data.departTicket.price && data.returnTicket.price">
-                {{data.returnTicket.price + data.departTicket.price }} €
+            <span v-else-if="departPrice && returnPrice">
+                {{ returnPrice + departPrice }} €
             </span>
         </h4>
     </div>
@@ -30,6 +30,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   name: 'FlightSummary',
   props: {
@@ -38,6 +39,37 @@ export default {
         return {}
       },
       type: Object
+    }
+  },
+  computed: {
+    departName: function () {
+      return this.$vnode.context.currentDepart.shortName
+    },
+    returnName: function () {
+      return this.$vnode.context.currentArrive.shortName
+    },
+    departDateFormatted: function () {
+      return this.$vnode.context.departDate
+    },
+    returnDateFormatted: function () {
+      return this.$vnode.context.returnDate
+    },
+    departPrice: function () {
+      return this.data.departTicket.price
+    },
+    returnPrice: function () {
+      return this.data.returnTicket.price
+    }
+  },
+  filters: {
+    // moment: function (returnDate) {
+    //   return moment(returnDate).format('YYYY-MM-DD')
+    // },
+    day: function (arriveDate) {
+      return moment(arriveDate).format('dddd, MMM Do YYYY')
+    },
+    minuteTime: function (departDate) {
+      return moment(departDate).format('HH:mm')
     }
   }
 }
@@ -55,7 +87,7 @@ export default {
   @include max-tablet(){
     margin: 15px 0;
     width: 100%;
-    padding: 40px 10px 0;
+    padding: 40px 0 0;
   }
   .summary-text{
     padding: 15px;
