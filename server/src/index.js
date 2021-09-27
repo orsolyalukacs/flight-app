a4f81b4ee2acdf60fcf2a21f74b98f22022c442b
orsolyalukacs <orchie.lucas@gmail.com>
2021-09-26 22:08:50 -0500

    add get requests to server and mock flights data

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+let flights = require('./data.json');
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/flights', (req, res) => {
+    return res.send(Object.values(flights));
+});
+
+app.get('/flights/:iata', (req, res) => {
+    return res.send(flights.filter(flight => flight["iata"] === req.params.iata));
+});
+
+app.get('/flights/:iata/:arrival/', (req, res) => {
+    const departure = flights.filter(flight => flight["iata"] === req.params.iata);
+    const connections = departure[0].connections;
+    const arrival = connections.filter(el => el["iata"] === req.params.arrival)
+    return res.send(arrival);
 });
 
 app.listen(process.env.PORT, () =>
